ec7ad044a1079f26a16c46fdd57d0abfd8c813cb
Marcelo Nicolas Manso <info@nicolasmanso.com.ar>
2015-01-11 18:26:33 -0300

    Play with templates and coverage

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from .views import home
+from .views import home, home_files
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,4 +9,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', home, name='home'),
+    url(r'^(?P<filename>(robots.txt)|(humans.txt))$',
+        home_files,
+        name='home-files'),
 )
